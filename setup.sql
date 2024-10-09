427d36af80f561aa0994e754164a25f681a868c7
Evan Riddick <riddickevan841@gmail.com>
2024-10-08 22:25:40 -0400

    Update setup.sql
    Create tables

--- setup.sql
+++ setup.sql
@@ -9,5 +9,25 @@ GRANT ALL ON passwords.* TO 'passwords_db_user'@'localhost';
 
 USE passwords;
 
+CREATE TABLE IF NOT EXISTS website (
+  website_name VARCHAR(128) NOT NULL,
+  website_url  VARCHAR(128) NOT NULL,
+  PRIMARY KEY  (website_url)
+);
+
+CREATE TABLE IF NOT EXISTS user (
+  website_url VARCHAR(128)  NOT NULL,
+  user_id     SMALLINT(5)  NOT NULL,
+  user_name   VARCHAR(128) NOT NULL,
+  PRIMARY KEY (website_url, user_id)
+);
+
+CREATE TABLE IF NOT EXISTS password (
+  website_url  VARCHAR(128)  NOT NULL,
+  user_id      SMALLINT(5)  NOT NULL,
+  password     VARBINARY(512) NOT NULL,
+  timestamp    DECIMAL(5,2) NOT NULL,
+  PRIMARY KEY (website_url, user_id, password)
+);
 
 source commands.sql;
